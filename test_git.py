b490da09cd9e94a408131e0c33576720048645ff
nnh242 <n.hoang@me.com>
2019-06-02 19:55:02 -0400

    fix conflict

--- test_git.py
+++ test_git.py
@@ -1,25 +1,15 @@
 SITES = [
-    "https://test1.com"
-    ""https://test1.com""
+    "https://test1.com",
+    "https://test2.com",
     "https://test3.com",
     "https://test4.com",
     "https://test5.com",
     "https://test6.com",
     "https://test7.com",
-    SITES = [
-    "https://test1.com"
-    ""https://test1.com""
-    "https://test3.com",
-    "https://test4.com",
-    "https://test5.com",
-    "https://test6.com",
-    "https://test7.com",
-    "https://test8.com",
-    "https://test9.com",
-    "https://test10.com"
-]
-
-    "https://test8.com",
-    "https://test9.com",
-    "https://test10.com"
+    "Your Name",
+    "Your Name",
+    "Your Name",
+    "Your Name",
+    "Your Name",
+    "Your Name",
 ]
