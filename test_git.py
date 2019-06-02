f772be584bc84312542db69bf3fbe8e3ea4379ba
nnh242 <n.hoang@me.com>
2019-06-02 19:36:48 -0400

    add 5

--- test_git.py
+++ test_git.py
@@ -1,8 +1,14 @@
 SITES = [
     "https://test1.com",
+    "https://test2.com",
     "https://test3.com",
     "https://test4.com",
     "https://test5.com",
     "https://test6.com",
-    "https://test7.com"
-]
+    "https://test7.com", 
+    "Tina Hoang",
+    "Tina Hoang",
+    "Tina Hoang",
+    "Tina Hoang",
+    "Tina Hoang",
+    ]
